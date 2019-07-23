dacc556eca78f82701c30ca3919640ab793b96b8
BK024 <bart.berkel@gmail.com>
2019-07-23 17:50:31 +0200

    Added a comment in the main class to clarify that what is there is just scratch code for now.

--- GameMain.py
+++ GameMain.py
@@ -2,6 +2,7 @@ from Dice import Dice
 
 if __name__ == "__main__":
 
+# TODO remove the code below. The code below is scratch code. Not the actual main. That will be implemented later.
     my_dice = Dice()
 
     for i in range(41):
