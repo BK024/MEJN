f3eb4bdc8a05c5b54e17c98bd763805021ad2aaa
BK024 <bart.berkel@gmail.com>
2019-07-23 18:15:46 +0200

    Just scratching with the Dice. In preparation of adding a doctest for the Dice.roll().

--- GameMain.py
+++ GameMain.py
@@ -1,10 +1,8 @@
 from Dice import Dice
 
 if __name__ == "__main__":
-
-# TODO remove the code below. The code below is scratch code. Not the actual main. That will be implemented later.
-    my_dice = Dice()
-
-    for i in range(41):
-        print("position_dict[{}] = Position({})".format(i, i))
-
+    # TODO remove the code below. The code below is scratch code. Not the actual main. That will be implemented later.
+    print("Just a scratch")
+    new_dice = Dice()
+    roll_value = new_dice.roll()
+    print("Let's roll the dice! \nYou rolled a {}".format(roll_value))
